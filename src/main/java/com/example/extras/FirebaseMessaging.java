96a5289bbf3422467c6f0a8472a954f485196675
Shyam <iamsv345@gmail.com>
2022-04-21 18:24:11 +0530

    commited

---
+++ src/main/java/com/example/extras/FirebaseMessaging.java
@@ -0,0 +1,8 @@
+package com.example.extras;
+
+public class FirebaseMessaging extends com.example.messaging.FirebaseMessaging {
+
+    public FirebaseMessaging(int x, boolean y) {
+        super(x, y);
+    }
+}
---
+++ src/main/java/com/example/messaging/FirebaseMessaging.java
@@ -0,0 +1,28 @@
+package com.example.messaging;
+
+public class FirebaseMessaging {
+
+    int x;
+    boolean y;
+
+    public FirebaseMessaging(int x, boolean y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public boolean isY() {
+        return y;
+    }
+
+    public void setY(boolean y) {
+        this.y = y;
+    }
+}
